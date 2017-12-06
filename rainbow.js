a3d921b90080b9fb8743c757085d4164c732778e
Mike Stamm <mike@heatlossgames.com>
2017-12-05 21:52:34 -0500

    Added gray border effect. Updated to latest API.

--- rainbow.js
+++ rainbow.js
@@ -1,25 +1,32 @@
-// var myImage = document.querySelector('img');
-
 var myRequest = new Request('https://sou01uewob.execute-api.us-east-1.amazonaws.com/get_rainbow_brights');
-var theDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var theMonths = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+var theDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var d = new Date();
-document.getElementById("todaysDate").innerHTML = theDays[d.getDay()] + ", " + theMonths[d.getMonth()] + " " + d.getDate()	;
+document.getElementById("todaysDate").innerHTML = theDays[d.getDay()] + ", " + theMonths[d.getMonth()] + " " + d.getDate();
 
-fetch(myRequest)
-  .then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    console.log('parsed json', json);
-    var obj=json.brights;
+var myRequest = new Request('https://wuq1fusv1h.execute-api.us-east-1.amazonaws.com/prod/');
+var brightBox = document.getElementById("brights");
 
-    Object.keys(obj).forEach(function(k){
-			console.log(k + ' - ' + obj[k].url);
-			var theBright  = document.createElement("img");
-			theBright.src = obj[k].url;
-			document.getElementById("brights").appendChild(theBright);
-		});
-  }).catch(function(ex) {
-    console.log('parsing failed', ex)
-  })
+fetch(myRequest).then(function(response) {
+  return response.json()
+}).then(function(json) {
+  console.log('parsed json', json);
+
+  var brightList = json.brights;
+
+  while (brightBox.lastChild) {
+    brightBox.removeChild(brightBox.lastChild);
+  }
+  
+  Object.keys(brightList).forEach(function(k) {
+    var brightLink = document.createElement("a");
+    var brightImg = document.createElement("img");
+    brightLink.appendChild(brightImg);
+    brightImg.href = brightList[k].url;
+    brightImg.src = brightList[k].image;
+    brightBox.appendChild(brightLink);
+  });
+}).catch (function(ex) {
+  console.log('parsing failed', ex)
+})
