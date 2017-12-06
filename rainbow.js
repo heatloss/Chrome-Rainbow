612217821a5af7f583639ec90f37bbe09546a9f9
Mike Stamm <luckbat@gmail.com>
2017-12-05 20:59:18 -0500

    Some resizing.

--- rainbow.js
+++ rainbow.js
@@ -1,6 +1,11 @@
 // var myImage = document.querySelector('img');
 
 var myRequest = new Request('https://sou01uewob.execute-api.us-east-1.amazonaws.com/get_rainbow_brights');
+var theDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var theMonths = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+var d = new Date();
+document.getElementById("todaysDate").innerHTML = theDays[d.getDay()] + ", " + theMonths[d.getMonth()] + " " + d.getDate()	;
 
 fetch(myRequest)
   .then(function(response) {
